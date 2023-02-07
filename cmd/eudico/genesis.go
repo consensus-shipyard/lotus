cbf9a13404d64a2632fa0b7cf300275a6d9a77e4
Denis Kolegov <dnkolegov@gmail.com>
2023-02-07 13:13:42 +0100

    remove cat command

--- cmd/eudico/genesis.go
+++ cmd/eudico/genesis.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"os"
 
@@ -25,7 +24,6 @@ var genesisCmd = &cli.Command{
 	Description: "manipulate lotus genesis template",
 	Subcommands: []*cli.Command{
 		genesisNewCmd,
-		genesisCatCmd,
 	},
 }
 
@@ -93,45 +91,5 @@ var genesisNewCmd = &cli.Command{
 		}
 
 		return os.Remove(cctx.Args().First() + ".tmp")
-	},
-}
-
-var genesisCatCmd = &cli.Command{
-	Name:        "cat",
-	Description: "reads and prints the genesis file",
-	Action: func(cctx *cli.Context) error {
-		if !cctx.Args().Present() {
-			return xerrors.New("genesis cat [genesis.json]")
-		}
-		fname := cctx.Args().First()
-		out, err := ioutil.ReadFile(fname)
-		if err != nil {
-			return xerrors.Errorf("failed to read genesis file %s: %w", fname, err)
-		}
-
-		var gen genesis.Template
-		if err = json.Unmarshal(out, &gen); err != nil {
-			return xerrors.Errorf("failed to decode genesis file: %w", err)
-		}
-
-		fmt.Println("Version: ", gen.NetworkVersion)
-		fmt.Println("Subnet ID: ", gen.NetworkName)
-		fmt.Printf("Remainder account: %s:%d:%s\n", gen.RemainderAccount.Type, gen.RemainderAccount.Balance, string(gen.RemainderAccount.Meta))
-		fmt.Printf("Root key: %s:%d:%s\n", gen.VerifregRootKey.Type, gen.VerifregRootKey.Balance, string(gen.VerifregRootKey.Meta))
-		fmt.Println("Timestamp: ", gen.Timestamp)
-		if len(gen.Miners) > 0 {
-			fmt.Println("Miners:")
-			for _, v := range gen.Miners {
-				fmt.Printf("\t- %s\n", v.ID)
-			}
-		}
-		if len(gen.Accounts) > 0 {
-			fmt.Println("Accounts:")
-			for _, v := range gen.Accounts {
-				fmt.Printf("\t- %s:%d:%s\n", v.Type, v.Balance, string(v.Meta))
-			}
-		}
-
-		return nil
 	},
 }
