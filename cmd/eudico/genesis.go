7765b0454423b999f277e9283c21889703742d87
Denis Kolegov <dnkolegov@gmail.com>
2023-02-08 23:02:44 +0100

    fix comments

--- cmd/eudico/genesis.go
+++ cmd/eudico/genesis.go
@@ -1,32 +1,17 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-
-	"github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli/v2"
 	"golang.org/x/xerrors"
 
 	"github.com/consensus-shipyard/go-ipc-types/types"
 
-	"github.com/filecoin-project/lotus/blockstore"
-	"github.com/filecoin-project/lotus/chain/vm"
-	"github.com/filecoin-project/lotus/genesis"
-	"github.com/filecoin-project/lotus/journal"
-	"github.com/filecoin-project/lotus/node/modules/testing"
-	"github.com/filecoin-project/lotus/storage/sealer/ffiwrapper"
-)
-
-const (
-	genesisTemplateFileName = "genesis/genesis.json"
+	"github.com/filecoin-project/lotus/eudico-core/genesis"
 )
 
 var genesisCmd = &cli.Command{
 	Name:        "genesis",
-	Description: "manipulate lotus genesis template",
+	Description: "manipulate eudico genesis template",
 	Subcommands: []*cli.Command{
 		genesisNewCmd,
 	},
@@ -34,7 +19,7 @@ var genesisCmd = &cli.Command{
 
 var genesisNewCmd = &cli.Command{
 	Name:        "new",
-	Description: "create new genesis from the template and store it in a car file",
+	Description: "create new genesis from the Spacenet template and store it in a car file",
 	Flags: []cli.Flag{
 		&cli.StringFlag{
 			Name:  "subnet-id",
@@ -55,47 +40,11 @@ var genesisNewCmd = &cli.Command{
 			return xerrors.Errorf("incorrect subnet ID %s: %w", sid, err)
 		}
 
-		e, err := os.Executable()
+		err = genesis.MakeGenesis(cctx.Context, cctx.String("out"), subnetID.String())
 		if err != nil {
-			return err
+			return xerrors.Errorf("failed to make genesis: %w", err)
 		}
 
-		tmplFilePath := filepath.Join(filepath.Dir(e), genesisTemplateFileName)
-		tmplBytes, err := ioutil.ReadFile(tmplFilePath)
-		if err != nil {
-			return xerrors.Errorf("failed to read template %s: %w", tmplFilePath, err)
-		}
-
-		var tmpl genesis.Template
-		if err := json.Unmarshal(tmplBytes, &tmpl); err != nil {
-			return err
-		}
-
-		tmpl.NetworkName = subnetID.String()
-
-		tmplBytes, err = json.MarshalIndent(&tmpl, "", "  ")
-		if err != nil {
-			return err
-		}
-
-		genFilePath, err := homedir.Expand(cctx.Args().First() + ".tmp")
-		if err != nil {
-			return err
-		}
-
-		if err := ioutil.WriteFile(genFilePath, tmplBytes, 0644); err != nil {
-			return xerrors.Errorf("failed to create genesis file %s: %w", genFilePath, err)
-		}
-
-		jrnl := journal.NilJournal()
-		bstor := blockstore.WrapIDStore(blockstore.NewMemorySync())
-		sbldr := vm.Syscalls(ffiwrapper.ProofVerifier)
-
-		_, err = testing.MakeGenesis(cctx.String("out"), genFilePath)(bstor, sbldr, jrnl)()
-		if err != nil {
-			return err
-		}
-
-		return os.Remove(cctx.Args().First() + ".tmp")
+		return nil
 	},
 }
