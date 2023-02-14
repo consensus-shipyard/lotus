ebf2d3b19d71d888c7a3c8e403fa42e2a2edcbc0
Denis Kolegov <dnkolegov@gmail.com>
2023-02-14 16:47:08 +0100

    update scripts

--- scripts/mir/update-valset.sh
+++ scripts/mir/update-valset.sh
@@ -2,7 +2,8 @@
 
 INDEX=$1
 
-cp -f ./configs/valset_$INDEX.json ~/.lotus-local-net0/mir.validators
-cp -f ./configs/valset_$INDEX.json ~/.lotus-local-net1/mir.validators
-cp -f ./configs/valset_$INDEX.json ~/.lotus-local-net2/mir.validators
-cp -f ./configs/valset_$INDEX.json ~/.lotus-local-net3/mir.validators
+cp -f ./mir-config/configs/valset_$INDEX.json ~/.lotus-local-net0/mir.validators
+cp -f ./mir-config/configs/valset_$INDEX.json ~/.lotus-local-net1/mir.validators
+cp -f ./mir-config/configs/valset_$INDEX.json ~/.lotus-local-net2/mir.validators
+cp -f ./mir-config/configs/valset_$INDEX.json ~/.lotus-local-net3/mir.validators
+cp -f ./mir-config/configs/valset_$INDEX.json ~/.lotus-local-net4/mir.validators
