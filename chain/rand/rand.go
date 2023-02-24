23cb24804757ebbb41d01b792bce21dc7a7fdfab
Denis Kolegov <dnkolegov@gmail.com>
2023-02-24 09:43:29 +0100

    uncomment debug log

--- chain/rand/rand.go
+++ chain/rand/rand.go
@@ -115,10 +115,9 @@ func NewStateRand(cs *store.ChainStore, blks []cid.Cid, b beacon.Schedule, netwo
 	// we don't use winningPoSt and we use fake windowPoSt when running
 	// mir consensus (at least for now)
 	if global.IsConsensusAlgorithm(global.MirConsensus) {
-		// FIXME DENIS - uncomment
-		// log.Warn("=================================================================================")
-		// log.Warn("DANGER ZONE! YOU ARE USING FAKE RANDOMNESS FOR YOUR VM AND PROOFS. USE WITH CARE!")
-		// log.Warn("=================================================================================")
+		log.Warn("=================================================================================")
+		log.Warn("DANGER ZONE! YOU ARE USING FAKE RANDOMNESS FOR YOUR VM AND PROOFS. USE WITH CARE!")
+		log.Warn("=================================================================================")
 		return &fakeRand{}
 	}
 
