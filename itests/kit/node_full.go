1c1ab4bd4bbabef80fccaea5e37dc99c2c233eef
Denis Kolegov <dnkolegov@gmail.com>
2023-03-02 21:03:12 +0100

    fix comments

--- itests/kit/node_full.go
+++ itests/kit/node_full.go
@@ -230,7 +230,7 @@ func (f *TestFullNode) IsSyncedWith(ctx context.Context, from abi.ChainEpoch, no
 	}
 
 	for _, n := range nodes {
-		ts, err := n.ChainGetTipSetByHeight(ctx, to, baseTipSet.Height())
+		ts, err := n.ChainGetTipSetByHeight(ctx, to, baseTipSet.Key())
 		if err != nil {
 			return 0, err
 		}
