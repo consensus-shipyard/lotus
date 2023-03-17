10ad1efa83d9b6568362836b0568f6fa6bad0666
Denis Kolegov <dnkolegov@gmail.com>
2023-03-17 15:45:07 +0100

    remove unused code

--- chain/consensus/mir/pool/pool.go
+++ chain/consensus/mir/pool/pool.go
@@ -2,50 +2,9 @@
 package pool
 
 import (
-	"github.com/filecoin-project/mir/pkg/dsl"
-	"github.com/filecoin-project/mir/pkg/modules"
 	"github.com/filecoin-project/mir/pkg/pb/requestpb"
 	requestpbtypes "github.com/filecoin-project/mir/pkg/pb/requestpb/types"
-
-	"github.com/filecoin-project/lotus/chain/consensus/mir/pool/handlers"
-	"github.com/filecoin-project/lotus/chain/consensus/mir/pool/types"
 )
-
-// ModuleConfig sets the module ids. All replicas are expected to use identical module configurations.
-type ModuleConfig = types.ModuleConfig
-
-// ModuleParams sets the values for the parameters of an instance of the protocol.
-// All replicas are expected to use identical module parameters.
-type ModuleParams = types.ModuleParams
-
-// DefaultModuleConfig returns a valid module config with default names for all modules.
-func DefaultModuleConfig() *ModuleConfig {
-	return &ModuleConfig{
-		Self:   "mempool",
-		Hasher: "hasher",
-	}
-}
-
-// DefaultModuleParams returns a valid module config with default names for all modules.
-func DefaultModuleParams() *ModuleParams {
-	return &ModuleParams{
-		MaxTransactionsInBatch: 512,
-	}
-}
-
-// NewModule creates a new instance of a request pool module implementation.
-func NewModule(ch chan chan []*requestpb.Request, mc *ModuleConfig, p *ModuleParams) modules.Module {
-	m := dsl.NewModule(mc.Self)
-
-	state := &types.State{
-		ReadyForTxsChan: ch,
-	}
-
-	handlers.IncludeComputationOfTransactionAndBatchIDs(m, mc, p)
-	handlers.IncludeBatchCreation(m, mc, p, state)
-
-	return m
-}
 
 type Fetcher struct {
 	ReadyForTxsChan chan chan []*requestpb.Request
