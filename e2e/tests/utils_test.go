89c66837912ea62750560e9a098b1157b9194619
Denis Kolegov <dnkolegov@gmail.com>
2023-03-25 20:55:19 +0100

    upd

--- e2e/tests/utils_test.go
+++ e2e/tests/utils_test.go
@@ -1,14 +1,12 @@
 package tests
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestFindRoot(t *testing.T) {
-	root, err := FindRoot()
+	_, err := FindRoot()
 	require.NoError(t, err)
-	require.True(t, strings.HasSuffix(root, "lotus"))
 }
