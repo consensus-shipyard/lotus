836a19b185713a6da169b3ff3f743c5106c790ef
Denis Kolegov <dnkolegov@gmail.com>
2023-05-17 18:11:25 +0300

    Use quic by default (#177)
    * use quic
    
    * update ipc scripts
    
    * Update scripts/ipc/src/root-single-validator.sh
    
    Co-authored-by: adlrocha <adlrocha@tutamail.com>
    
    * mark udp ports
    
    ---------
    
    Co-authored-by: adlrocha <adlrocha@tutamail.com>

--- scripts/ipc/run-subnet-docker.sh
+++ scripts/ipc/run-subnet-docker.sh
@@ -16,7 +16,7 @@ VAL_KEY_ABSOLUTE_PATH=$4
 CONTAINER_NAME=`echo ipc${SUBNETID}_${PORT} | sed 's/\//_/g'`
 
 echo "[*] Running docker container for root in port $PORT"
-img=`docker run -dit --add-host host.docker.internal:host-gateway -p $PORT:1234 -p $VAL_PORT:1347 -v $VAL_KEY_ABSOLUTE_PATH:/wallet.key:ro --name $CONTAINER_NAME --entrypoint "/scripts/ipc/entrypoints/eudico-subnet.sh" eudico $SUBNETID`
+img=`docker run -dit --add-host host.docker.internal:host-gateway -p $PORT:1234 -p $VAL_PORT:1348/udp -v $VAL_KEY_ABSOLUTE_PATH:/wallet.key:ro --name $CONTAINER_NAME --entrypoint "/scripts/ipc/entrypoints/eudico-subnet.sh" eudico $SUBNETID`
 echo "[*] Waiting for the daemon to start"
 docker exec -it $img  eudico wait-api --timeout 350s
 sleep 10
@@ -27,6 +27,6 @@ echo ">>> Token to $SUBNETID daemon: $token"
 wallet=`docker exec -it $img  eudico wallet default`
 echo ">>> Default wallet: $wallet"
 echo ">>> Subnet subnet validator info:"
-docker exec -it $img eudico mir validator config validator-addr | grep '127.0.0.1/tcp' | sed 's/^.*\/127.0.0.1/\/dns\/host.docker.internal/' | sed -E "s/\/[0-9]+\//\/$VAL_PORT\//"
+docker exec -it $img eudico mir validator config validator-addr | grep '127.0.0.1/udp' | sed 's/^.*\/127.0.0.1/\/dns\/host.docker.internal/' | sed -E "s/\/[0-9]+\//\/$VAL_PORT\//"
 echo ">>> API listening in host port $PORT"
 echo ">>> Validator listening in host port $VAL_PORT"
