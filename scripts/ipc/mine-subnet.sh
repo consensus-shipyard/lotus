d320852b60c1b1187da4891c73469155724f2525
Denis Kolegov <dnkolegov@gmail.com>
2023-08-01 18:59:25 +0200

    update script

--- scripts/ipc/mine-subnet.sh
+++ scripts/ipc/mine-subnet.sh
@@ -1,15 +1,22 @@
-
 #!/usr/bin/env bash
-set -e
 
-if [ $# -ne 1 ]
+if [ $# -lt 1 ] || [ $# -gt 2 ]
 then
-    echo "Provide the running container for the validator as an argument"
-    echo "Args: [validator container]"
+    echo "Provide the running container for the validator as an argument."
+    echo "mine-subnet.sh [--debug] containerID"
     exit 1
 fi
 
-IMG=$1
+if [ "$1" = "--debug" ]; then
+  if [ "$2" = "" ]; then
+    echo "Empty containerID"
+    exit
+  fi
+    echo "[*] Starting to mine on validator in debug mode"
+    docker exec -e MIR_INTERCEPTOR_OUTPUT="mir-event-logs" -it $2 eudico mir validator run --membership=onchain --nosync --ipcagent-url=http://host.docker.internal:3030/json_rpc
+else
+    echo "[*] Starting to mine on validator"
+    docker exec -it $1 eudico mir validator run --membership=onchain --nosync --ipcagent-url=http://host.docker.internal:3030/json_rpc
+fi
 
-echo "[*] Starting to mine on validator"
-docker exec -e MIR_INTERCEPTOR_OUTPUT="mir-event-logs" -it $IMG eudico mir validator run --membership=onchain --nosync --ipcagent-url=http://host.docker.internal:3030/json_rpc
+
